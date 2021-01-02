2060185d2ff00e491b39653ecf305c0f42064851
DusanSekulic9 <dukiseki@gmail.com>
2021-01-02 23:16:23 +0100

    Commit za kraj dana.

--- Servis-Avionskih-Karata/src/main/java/app/controller/Controller.java
+++ Servis-Avionskih-Karata/src/main/java/app/controller/Controller.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.forms.LetForm;
 import app.repositories.KarteRepository;
 
 @RestController
@@ -14,20 +17,21 @@ import app.repositories.KarteRepository;
 public class Controller {
 
 	private KarteRepository karteRepo;
-	
+
 	@Autowired
 	public Controller(KarteRepository karteRepo) {
 		this.karteRepo = karteRepo;
 	}
-	
+
 	@PostMapping("/kupiKartu")
-	public ResponseEntity<String> kupiKartu(){
+	public ResponseEntity<String> kupiKartu(@RequestBody LetForm let,
+			@RequestHeader(value = "Authorization") String token) {
 		try {
 			
 			
-			
+
 			return new ResponseEntity<String>("Uspesno kupljena karta", HttpStatus.ACCEPTED);
-		}catch(Exception e){
+		} catch (Exception e) {
 			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
 		}
 	}
---
+++ Servis-Avionskih-Karata/src/main/java/app/forms/LetForm.java
@@ -0,0 +1,54 @@
+package app.forms;
+
+
+public class LetForm {
+
+	
+	
+	private String avion;
+	private String pocetnaDestinacija;
+	private String krajnjaDestinacija;
+	private String duzinaLeta;
+	private int cena;
+
+	public String getAvion() {
+		return avion;
+	}
+
+	public void setAvion(String avion) {
+		this.avion = avion;
+	}
+
+	public String getPocetnaDestinacija() {
+		return pocetnaDestinacija;
+	}
+
+	public void setPocetnaDestinacija(String pocetnaDestinacija) {
+		this.pocetnaDestinacija = pocetnaDestinacija;
+	}
+
+	public String getKrajnjaDestinacija() {
+		return krajnjaDestinacija;
+	}
+
+	public void setKrajnjaDestinacija(String krajnjaDestinacija) {
+		this.krajnjaDestinacija = krajnjaDestinacija;
+	}
+
+	public String getDuzinaLeta() {
+		return duzinaLeta;
+	}
+
+	public void setDuzinaLeta(String duzinaLeta) {
+		this.duzinaLeta = duzinaLeta;
+	}
+
+	public int getCena() {
+		return cena;
+	}
+
+	public void setCena(int cena) {
+		this.cena = cena;
+	}
+
+}
